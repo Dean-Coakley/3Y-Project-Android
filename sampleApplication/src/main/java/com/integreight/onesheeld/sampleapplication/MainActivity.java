1aa9e2ab693a2829ed06ac844405e39e7adfc2e1
Alex <alexsoregan@gmail.com>
2017-09-24 14:45:42 +0100

    switches added instead of Toggles, connection handling improved

--- sampleApplication/src/main/java/com/integreight/onesheeld/sampleapplication/MainActivity.java
+++ sampleApplication/src/main/java/com/integreight/onesheeld/sampleapplication/MainActivity.java
@@ -9,13 +9,20 @@ package com.integreight.onesheeld.sampleapplication;
 */
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -33,12 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
     //GUI
     private Button scanButton;
-    private ToggleButton toggleLights;
-    private ToggleButton toggleHeating;
+    private Switch toggleLights;
+    private Switch toggleHeating;
+    private Button disconnectButton;
 
     //Sheeld
     private OneSheeldManager manager;
     private OneSheeldDevice sheeldDevice;
+
     private OneSheeldScanningCallback scanningCallback = new OneSheeldScanningCallback() {
         @Override
         public void onDeviceFind(OneSheeldDevice device){
@@ -48,15 +57,19 @@ public class MainActivity extends AppCompatActivity {
             device.connect();
         }
     };
+
+
     private OneSheeldConnectionCallback connectionCallback = new OneSheeldConnectionCallback() {
         @Override
         public void onConnect(OneSheeldDevice device) {
             sheeldDevice = device;
 
+            // when a connection is established, enable device-specific buttons
             runOnUiThread(new Runnable() {@Override public void run()
                 {
                     toggleHeating.setEnabled(true);
                     toggleLights.setEnabled(true);
+                    disconnectButton.setEnabled(true);
                 }
             });
         }
@@ -64,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         public void onDisconnect(OneSheeldDevice device) {
             sheeldDevice = null;
 
+            //when a disconnect occurs, make sure all device-specific buttons are disabled
             runOnUiThread(new Runnable() {@Override public void run()
             {
                 toggleHeating.setEnabled(true);
@@ -80,11 +94,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //setup
+        //setup - initialise SDK, then set connection retry-counts and timeouts
         OneSheeldSdk.init(this);
         OneSheeldSdk.setDebugging(true);
         manager = OneSheeldSdk.getManager();
         manager.setConnectionRetryCount(1);
+        manager.setScanningTimeOut(5);
         manager.setAutomaticConnectingRetriesForClassicConnections(true);
         // add callback functions for handling connections / scanning
         manager.addConnectionCallback(connectionCallback);
@@ -95,19 +110,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //cleanly disconnect all devices if app is nearly destruction
+    @Override
+    protected void onDestroy(){
+        manager.disconnectAll();
+        manager.cancelConnecting();
+        manager.cancelScanning();
+
+        super.onDestroy();
+    }
+
+
     // GUI SETUP
     public void setupGUI() {
         scanButton = (Button) findViewById(R.id.scanButton);
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
+            //cancel all existing scans / connections in progress befroe rescanning
             public void onClick(View v) {
+                manager.cancelScanning();
+                manager.cancelConnecting();
                 manager.scan();
+            }
+        });
 
+        //disconnects all devices
+        disconnectButton = (Button) findViewById(R.id.disconnectButton);
+        disconnectButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                manager.disconnectAll();
+                disconnectButton.setEnabled(false);
             }
         });
 
         // add heating toggling functionality
-        toggleHeating = (ToggleButton) findViewById(R.id.toggle_heating);
+        toggleHeating = (Switch) findViewById(R.id.toggle_heating);
         toggleHeating.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -120,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // add lighting toggling functionality
-        toggleLights = (ToggleButton) findViewById(R.id.toggle_lights);
+        toggleLights = (Switch) findViewById(R.id.toggle_lights);
         toggleLights.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -132,8 +170,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // all device-specific buttons are disabled by default
         toggleHeating.setEnabled(false);
         toggleLights.setEnabled(false);
+        disconnectButton.setEnabled(false);
     }
 
 
